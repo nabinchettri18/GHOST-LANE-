b4d0e86d6d1fbe1a422115c84f178434793e4d2d
Nabin Chettri <nabinchettri0007@gmail.com>
2026-09-13 18:22:56 +0530

    Add contracts workspace page

---
+++ app/contracts/page.tsx
@@ -0,0 +1,2 @@
+import { DataPage } from '@/components/data-page'
+export default function Contracts(){return <DataPage title="Contracts" description="Manage committed freight capacity and measure whether contracted volume materializes." columns={['Contract','Lane','Carrier','Volume','Rate','Start','End','Realization']}/>}
